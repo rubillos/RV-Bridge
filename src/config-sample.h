39e879bc3d7d6ed6fa3d1f31a57c7bd0f32b809f
Randy Ubillos <randy@mac.com>
2023-02-21 14:26:50 -0800

    Added MIT license

--- src/config-sample.h
+++ src/config-sample.h
@@ -1,3 +1,30 @@
+/*********************************************************************************
+ *  MIT License
+ *  
+ *  Copyright (c) 2023 Randy Ubillos
+ *  
+ *  https://github.com/rubillos/RV-Bridge
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *  
+ ********************************************************************************/
+ 
 #include <stdint.h>
 
 // #define OVERRIDE_MAC_ADDRESS {0x53, 0xC3, 0xD3, 0xBD, 0x20, 0x51}
@@ -7,4 +34,4 @@ constexpr const char* sspwd = "***";
 
 constexpr uint8_t sourceAddress = 145;
 
-#include "Miramar3202.h"
+#include "RV/Miramar_2020_3202.h"
